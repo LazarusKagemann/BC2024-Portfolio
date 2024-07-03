8744211fc79e169fc9afeb71b4ab6cb9872aff27
Lazer <lazaruskagemann@gmail.com>
2024-07-03 07:33:00 -0400

    w

--- script2.js
+++ script2.js
@@ -1,14 +1,13 @@
-// script2.js
+document.addEventListener("DOMContentLoaded", function () {
+  const banner = document.querySelector(".banner");
+  const toggleBannerBtn = document.getElementById("toggleBannerBtn");
 
-function toggleBanner() {
-    const banner = document.querySelector('.banner');
-    banner.classList.toggle('hidden');
-}
-window.onload = function() {
-    window.micAccessTool = new MicAccessTool({
-      link: 'http://your-awesome-website.com/your-accessibility-declaration.pdf',
-      contact: 'mailto:your-mail@your-awesome-website.com',
-      buttonPosition: 'right', // default is 'left'
-      forceLang: 'ru-RU' // default is 'en' may be 'he-IL', 'ru-RU', or 'fr_FR'
-    });
-  }
+  toggleBannerBtn.addEventListener("click", function () {
+    banner.classList.toggle("hidden");
+    if (banner.classList.contains("hidden")) {
+      toggleBannerBtn.textContent = "+";
+    } else {
+      toggleBannerBtn.textContent = "-";
+    }
+  });
+});
